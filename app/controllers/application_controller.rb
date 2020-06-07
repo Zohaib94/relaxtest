e9c51fd39eb480999a8406abf5c0e0ca857bd832
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-06-07 11:49:00 +0300

    Application: Handling record not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_not_found
+    redirect_to root_path, notice: 'Record not found, please try again'
+  end
 end
