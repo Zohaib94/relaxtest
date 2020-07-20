6e2c7d5b83f5f5773b850c4e0d55adcf2f830af3
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-07-20 20:07:14 +0300

    Log Receipt: Added Admin views

---
+++ app/admin/log_receipts.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register LogReceipt do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :invoiced_at, :weight, :average_diameter, :quantity, :average_weight, :average_length, :amount
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :invoiced_at, :weight, :average_diameter, :quantity, :average_weight, :average_length, :amount]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
