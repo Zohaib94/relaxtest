e50c63ec4b4685e8a839796fbb34360eaffe8785
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-07-20 21:13:26 +0300

    Log Receipt: Updated ActiveAdmin Views and Added File Upload

--- app/admin/log_receipts.rb
+++ app/admin/log_receipts.rb
@@ -1,18 +1,62 @@
 ActiveAdmin.register LogReceipt do
+  form html: { enctype: "multipart/form-data" } do |f|
+    f.inputs "LogReceipt", multipart: true do
+      f.input :invoiced_at, as: :datepicker, start_year: 1900
+      f.input :amount
+      f.input :quantity
+      f.input :average_weight
+      f.input :average_diameter
+      f.input :average_length
+      f.input :user, as: :select, collection: User.all.pluck(:email, :id)
+      f.input :log_image, as: :file
+    end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :user_id, :invoiced_at, :weight, :average_diameter, :quantity, :average_weight, :average_length, :amount
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:user_id, :invoiced_at, :weight, :average_diameter, :quantity, :average_weight, :average_length, :amount]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+    f.actions
+  end
+
+  index do
+    column :id
+    column :invoiced_at
+    column :amount
+    column :quantity
+    column :average_weight
+    column :average_diameter
+    column :average_length
+
+    column 'User' do |log_receipt|
+      log_receipt.user_email
+    end
+
+    column 'Log Image' do |log_receipt|
+      div do
+        image_tag(log_receipt.thumbnail)
+      end
+    end
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :invoiced_at
+      row :amount
+      row :quantity
+      row :average_weight
+      row :average_diameter
+      row :average_length
   
+      row 'User' do |log_receipt|
+        log_receipt.user_email
+      end
+  
+      row 'Log Image' do |log_receipt|
+        div do
+          image_tag(log_receipt.thumbnail)
+        end
+      end
+    end
+  end
+
+  permit_params :user_id, :invoiced_at, :average_diameter, :quantity, :average_weight, :average_length, :amount, :log_image
 end
--- app/models/log_receipt.rb
+++ app/models/log_receipt.rb
@@ -1,3 +1,15 @@
 class LogReceipt < ApplicationRecord
+  THUMBNAIL_WIDTH = 150
+
   belongs_to :user
+
+  has_one_attached :log_image
+
+  def user_email
+    user.email
+  end
+
+  def thumbnail
+    log_image.variant(resize_to_limit: [THUMBNAIL_WIDTH, nil])
+  end
 end
